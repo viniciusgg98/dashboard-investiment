5a291667c016e30eecc52c0344baea473c460e5a
Vinícius Ghilardi <viniciusgg98@gmail.com>
2025-03-29 17:58:16 -0300

    Minha ultima commit com o codigo funcional

--- investimento.py
+++ investimento.py
@@ -2,10 +2,11 @@
 aporte_mensal = float(input("Quanto você vai aportar por mês R$: "))
 anos = int(input("Quantos anos você vai investir: "))
 taxa_de_rendimento_anual = float(input("Qual sua taxa de rendimento anual: "))
-taxa_de_rendimento_mensal = taxa_de_rendimento_anual/12
-patrimonio = float(input("Quanto dinheiro você já tem investido: R$"))
+taxa_de_rendimento_mensal = (taxa_de_rendimento_anual / 100) / 12
+patrimonio = float(input("Quanto dinheiro você já tem investido R$: "))
 
 for mes in range(anos *12):
     patrimonio = (patrimonio + aporte_mensal) * (1+taxa_de_rendimento_mensal)
 
-print(f"saldo final após {anos} anos: R$ {patrimonio}")
+patrimonio_formatado = "{:,.2f}".format(patrimonio)
+print(f"saldo final após {anos} anos: R$ {patrimonio_formatado}")
