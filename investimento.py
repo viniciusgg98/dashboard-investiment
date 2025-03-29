26737d8fa1aa3b867a0dd401ec40d2328d5a0a01
Vinícius Ghilardi <viniciusgg98@gmail.com>
2025-03-29 17:40:55 -0300

    adicionei variaveis a investimento.py

--- investimento.py
+++ investimento.py
@@ -1,1 +1,6 @@
 #Titulo do meu projeto
+aporte_mensal = float(input("Quanto você vai aportar por mês R$: "))
+anos = int(input("Quantos anos você vai investir: "))
+taxa_de_rendimento_anual = float(input("Qual sua taxa de rendimento anual: "))
+taxa_de_rendimento_mensal = taxa_de_rendimento_anual/12
+patrimonio = float(input("Quanto dinheiro você já tem investido: R$"))
